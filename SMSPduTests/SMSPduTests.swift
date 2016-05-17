6b3be9bc82814bc898ba2fd22bb26e8c5dfa2358
Pavle Mijatovic <paul.mijatovic@gmail.com>
2016-05-17 11:54:17 +0200

    Testing

--- SMSPduTests/SMSPduTests.swift
+++ SMSPduTests/SMSPduTests.swift
@@ -10,27 +10,39 @@ import XCTest
 @testable import SMSPdu
 
 class SMSPduTests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testNumberEncoder() {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    XCTAssert(true)
+  }
+  
+  
+  func testNumberDecoder() {
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+    XCTAssert(true)
+  }
+  
+  
+  func testData7BitEncoder() {
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+  }
+  
+  func testData7BitDecoder() {
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
+  
+  func test
+  
+  
 }
